d33dabe0322f8c843cf10be4f1f98f5e0f2f672f
huong <huynhhuyenhuong2006@gmail.com>
2025-11-26 16:51:24 +0700

    update

---
+++ b14/bai5.php
@@ -0,0 +1,27 @@
+<?php
+    $ck_name = "hoten";
+    $ck_value = "Huỳnh Huyền Hương";
+    $ck_value_new = "Huỳnh Huyền Hương 1234";
+    setcookie ($ck_name, $ck_value_new, time()-(86400*20),"/")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if(!isset($_COOKIE[$ck_name])){
+            echo "Cookie đã được xoá";
+        }
+        else {
+            echo "Cookie vẫn còn";
+            echo "<br> Với giá trị là: $ck_value_new";
+        }
+
+    ?>
+</body>
+</html>
---
+++ b14/bai6.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    // Tạo giá trị
+    $SESSION ["hoten"]="Hương";
+    $SESSION ["diachi"]="CM";
+    echo "Các giá trị đã được tạo ";
+
+?>
