40d7283d801ea611e6a0629da5e41c81491cd9a4
huong <huynhhuyenhuong2006@gmail.com>
2025-12-17 13:29:47 +0700

    update

---
+++ b17/bai1.php
@@ -0,0 +1,20 @@
+<?php
+    class Hocsinh {
+        // Khai báo các thuộc tính
+        var $ten;
+        var $tuoi;
+        var $diem;
+        // Khai báo phương thức
+        function Hoc (){
+            // Code
+        }
+        function Thi (){
+
+        }
+        function ThongtinHS (){
+            echo "Tên học sinh: " . $this-> ten;
+            echo "Tuổi học sinh: " . $this-> tuoi;
+            echo "Điểm học sinh: " . $this-> diem;
+        }
+    }
+?>
