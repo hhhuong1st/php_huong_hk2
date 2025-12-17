31da1a86f8a70e30d4d875cd7ecffe6b5a05e6d9
huong <huynhhuyenhuong2006@gmail.com>
2025-12-17 13:42:16 +0700

    a

--- b17/bai1.php
+++ b17/bai1.php
@@ -13,8 +13,25 @@
         }
         function ThongtinHS (){
             echo "Tên học sinh: " . $this-> ten;
-            echo "Tuổi học sinh: " . $this-> tuoi;
-            echo "Điểm học sinh: " . $this-> diem;
+            echo "<br>Tuổi học sinh: " . $this-> tuoi;
+            echo "<br>Điểm học sinh: " . $this-> diem;
         }
     }
+    // Tạo đối tượng 
+    $A = new Hocsinh();
+    $A -> ten = "Nguyễn Văn A";
+    $A -> tuoi = 19;
+    $A -> diem = 10;
+    // Gọi
+    $A -> ThongtinHS();
+
+
+    // Tạo đối tượng 
+    echo "<hr>";
+    $B = new Hocsinh();
+    $B -> ten = "Nguyễn Văn B";
+    $B -> tuoi = 19;
+    $B -> diem = 10;
+    // Gọi
+    $B -> ThongtinHS();
 ?>
